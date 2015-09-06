d6f13ffec5de730cf8ebd7c46003eda21f5baef8
juzipi <dajuzipi@gamil.com>
2015-09-06 17:30:06 +0800

    添加一般体形

--- common/js/view.js
+++ common/js/view.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){ 
 
- 
+  	FastClick.attach(document.body);
 
 var _defult={
 	// imgauto height width
@@ -42,6 +42,19 @@ var _defult={
 		  }
 
  });
+	},
+	//弹出框图片垂直居中对齐
+	heightCenter : function(el){
+		var documentHeight = $(window).height();
+		var elHeight = el.height();
+		var half = elHeight/2; //通过margin-top负值，居中对齐。
+		if(elHeight<documentHeight){//当图片高度小于窗口高度
+			el.css({"margin-top":-half});
+		}else{//当图片高度大于窗口高度
+			el.css({"width":"auto","height":documentHeight,"position":"relative","top":"auto"});
+			$(document).find('body').css("overflow","hidden");//不让下面的内容可以滚动。
+		}
+		
 	}
 }
 _defult.imgAuto("#headImage");
@@ -49,20 +62,55 @@ _defult.imgAuto("#headImage");
 
 
 
+
 // 点击图片放大效果
-$(".head-nobg").on("tap",".image",function(){
-	var viewport = document.querySelector("meta[name=viewport]");
-	var url = $(this).attr("src");
-	var html = '<div class="img-popup" id="J_imgPopup"><img src='+url+' alt=""  /><div class="maskblack"></div></div>'
-	viewport.setAttribute('content', 'width=device-width');
-	$(this).parent().parent().before().append(html);
-})
+// $(".head-nobg").on("tap",".image",function(){
+// 	var viewport = document.querySelector("meta[name=viewport]");
+// 	var url = $(this).attr("src");
+// 	var html = '<div class="img-popup" id="J_imgPopup"><img src='+url+' alt=""  /><div class="maskblack"></div></div>'
+// 	viewport.setAttribute('content', 'width=device-width');
+// 	$(this).parent().parent().before().append(html);
+// })
 	// 关闭图片
-$(".bd").on("tap","#J_imgPopup", function(e) {
-	$(this).remove();
+$(document).on("click",".maskblack", function(e) {
+	$("#J_imgPopup").hide();
 });
 
-
+// 单例
+var singleFn = function(fn){
+	var rel;
+	return function(){
+		return rel || (rel = fn.apply(this,arguments));
+	}
+}
+// 创建img弹出框
+var CreateImgPopup = function(url){
+	var viewport = document.querySelector("meta[name=viewport]");
+	var popup = document.createElement('div');
+	popup.id = "J_imgPopup";
+	popup.className = "img-popup";
+	viewport.setAttribute('content', 'width=device-width');
+	$(document).find('body').append(popup);
+	return popup;
+}
+var creatImgPopup = singleFn(CreateImgPopup);
+//约定data-popup带属性的具有弹出img
+$("[data-popup]").on('click', function(event) {
+	var url = $(this).attr("src");
+	var popup = creatImgPopup();
+	var image = document.createElement("img");
+	image.src = url;
+	image.id = "J_popupImg";
+	// image.src = "../../common/img/icon_plan.png";
+	var html = '<div class="i-loading"><div><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span></div></div><div class="maskblack"></div>'
+	$(popup).html(html);
+	$(popup).show();
+	image.onload = function(){
+		$(popup).find('.i-loading').hide();
+		$(popup).append(image);
+		_defult.heightCenter($("#J_popupImg"));
+	}
+});
 
 
 	//全局所有元素切换hover类	
