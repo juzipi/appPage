467fe863fe0bfcf65ae2750314c9bde369dd4c23
juzipi <dajuzipi@gamil.com>
2016-04-08 14:35:48 +0800

    20160407
    1、修改医院看病价格
    2、添加活动领奖页面

--- common/js/priceList.js
+++ common/js/priceList.js
@@ -1,55 +1,55 @@
-$.ajax({
-				url:"../../common/data/priceClass.json",
-				dataType:"json",
-				beforeSend : function(){
-					console.log("before")
-				},
-				complete : function(d){
-					var s = d.response;
-					var data = $.parseJSON(s);
-					// console.log(d.response);
-				var Arr_item = [];
-			$.each(data,function(index,item){
-				// 建立最顶部div
-				var div = document.createElement("div");
-				div.setAttribute("id", index);
-				div.setAttribute("class", "none");
+// $.ajax({
+// 				url:"../../common/data/priceClass.json",
+// 				dataType:"json",
+// 				beforeSend : function(){
+// 					console.log("b")
+// 				},
+// 				complete : function(d){
+// 					var s = d.response;
+// 					var data = $.parseJSON(s);
+// 					console.log(d.response);
+// 				var Arr_item = [];
+// 			$.each(data,function(index,item){
+// 				// 建立最顶部div
+// 				var div = document.createElement("div");
+// 				div.setAttribute("id", index);
+// 				div.setAttribute("class", "none");
 				
-				var parentUl = document.createElement("ul");
-				parentUl.setAttribute("class", "priceList");
+// 				var parentUl = document.createElement("ul");
+// 				parentUl.setAttribute("class", "priceList");
 
-				$.each(item,function(i,t){
-					var _a='';//li这是第二级别需要的li
-					_a += '<div class="parentList">'+ t +'<i class="icon-angle-right"></i></div>';	
-					Arr_item.push(t);	
-					var parentLi = document.createElement("li");
-					$(parentLi).append(_a);
-					$.get("../../common/data/priceList.json",function(listData){
+// 				$.each(item,function(i,t){
+// 					var _a='';//li这是第二级别需要的li
+// 					_a += '<div class="parentList">'+ t +'<i class="icon-angle-right"></i></div>';	
+// 					Arr_item.push(t);	
+// 					var parentLi = document.createElement("li");
+// 					$(parentLi).append(_a);
+// 					$.get("../../common/data/priceList.json",function(listData){
 
-								var chlidUl = document.createElement("ul");
-										chlidUl.setAttribute("class", "priceDetail");
-										$.each(listData[t],function(index,item){
-											var chlidLi = '';
-											chlidLi += '<li><div class="priceTitle">'+item.name+'</div><sub class="font-c-gray">'+item.remark+'</sub><div class="price">'+item.price+'</div></li> ';
-											$(chlidUl).append(chlidLi);
-										})
+// 								var chlidUl = document.createElement("ul");
+// 										chlidUl.setAttribute("class", "priceDetail");
+// 										$.each(listData[t],function(index,item){
+// 											var chlidLi = '';
+// 											chlidLi += '<li><div class="priceTitle">'+item.name+'</div><sub class="font-c-gray">'+item.remark+'</sub><div class="price">'+item.price+'</div></li> ';
+// 											$(chlidUl).append(chlidLi);
+// 										})
 										
-									$(parentLi).append(chlidUl);
+// 									$(parentLi).append(chlidUl);
 									
-						})
+// 						})
 					
-					$(parentUl).append(parentLi);
-				})
+// 					$(parentUl).append(parentLi);
+// 				})
 				
-				$(div).append(parentUl)
+// 				$(div).append(parentUl)
 				
-				$("#priceClassList").append(div)
-			})
+// 				$("#priceClassList").append(div)
+// 			})
 				
-				var url = window.location.hash;
-				$(url).show().siblings().hide();
-				}
-			})
+// 				var url = window.location.hash;
+// 				$(url).show().siblings().hide();
+// 				}
+// 			})
 
 		$(document).ready(function(){ 
 				$(document).on("click",".parentList",function(){
